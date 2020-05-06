b02bb18e15810c7fc0cd4c3cefccda052599e4d3
Mark Swaffer <mdswaffer@gmail.com>
2020-05-06 15:05:20 +0100

    Adds script to help determine test status on Jenkins

---
+++ check-test-status.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+tests_failed=$(grep -c scenario.*failed test-results.txt)
+
+echo "Result is $tests_failed"
+
+if [ "$tests_failed" -eq "0" ];
+then
+  echo "Tests all passed"
+else
+  echo "Tests have failed"
+fi
